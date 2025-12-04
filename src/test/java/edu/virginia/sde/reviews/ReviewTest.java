d5dcfbdc192e07a96c179099878b34556d014763
Josh Riley <arv4xc@virginia.edu>
2025-12-04 17:58:32 -0500

    Wrote tests for getReviewsFromCourse

---
+++ src/test/java/edu/virginia/sde/reviews/ReviewTest.java
@@ -0,0 +1,70 @@
+package edu.virginia.sde.reviews;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.sql.Timestamp;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ReviewTest {
+
+    @BeforeEach
+    public void clearDB() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createQuery("delete from Review").executeUpdate();
+        transaction.commit();
+        session.close();
+    }
+
+    @Test
+    public void getReviewsFromCourse_true() {
+        Course course = new Course("CS", 1550, "Intro");
+        Course otherCourse = new Course("CS", 1650, "Not Intro");
+        Profile profile1 = new Profile("user", "pass");
+        Profile profile2 = new Profile("user1", "pass");
+        Profile profile3 = new Profile("user2", "pass");
+        Course.insertCourse(course);
+        Course.insertCourse(otherCourse);
+        Profile.insertProfile(profile1);
+        Profile.insertProfile(profile2);
+        Profile.insertProfile(profile3);
+
+        Review review1 = new Review(
+                profile1,
+                course,
+                5,
+                "great",
+                new Timestamp(System.currentTimeMillis()).toString());
+        Review review2 = new Review(
+                profile2,
+                course,
+                1,
+                "horrible",
+                new Timestamp(System.currentTimeMillis()).toString());
+        Review review3 = new Review(
+                profile3,
+                otherCourse,
+                3,
+                "okay",
+                new Timestamp(System.currentTimeMillis()).toString());
+
+        Review.insertReview(review1);
+        Review.insertReview(review2);
+        Review.insertReview(review3);
+
+        assertTrue(Review.getReviewsFromCourse(course).equals(List.of(review1, review2)));
+    }
+
+    @Test
+    public void getReviewsFromCourse_false_empty() {
+        Course course = new Course("CS", 1550, "Intro");
+        Course.insertCourse(course);
+
+        assertTrue(Review.getReviewsFromCourse(course).isEmpty());
+    }
+}
