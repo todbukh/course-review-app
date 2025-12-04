3af7daa36d224641450d36a8aa2715b4ce762ba5
Josh Riley <arv4xc@virginia.edu>
2025-12-04 18:14:40 -0500

    Equivalence test for courseExists

--- src/test/java/edu/virginia/sde/reviews/CourseTest.java
+++ src/test/java/edu/virginia/sde/reviews/CourseTest.java
@@ -1,4 +1,30 @@
 package edu.virginia.sde.reviews;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CourseTest {
+
+
+    @BeforeEach
+    public void clearDB() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createQuery("delete from Course").executeUpdate();
+        transaction.commit();
+        session.close();
+    }
+
+    @Test
+    public void courseExists(){
+        Course course = new Course("CS", 1000, "Intro to Computer Science");
+        Course.insertCourse(course);
+
+        assertTrue(Course.courseExists(course));
+    }
+
 }
