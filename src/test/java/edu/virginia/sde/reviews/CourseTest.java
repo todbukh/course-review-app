5bb5a0f31a0e664a7ecf72d14f0244cb6084e2bd
Josh Riley <arv4xc@virginia.edu>
2025-12-05 13:06:26 -0500

    Equivalence test for getCourseList

--- src/test/java/edu/virginia/sde/reviews/CourseTest.java
+++ src/test/java/edu/virginia/sde/reviews/CourseTest.java
@@ -5,6 +5,8 @@ import org.hibernate.Transaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CourseTest {
@@ -25,6 +27,20 @@ public class CourseTest {
         Course.insertCourse(course);
 
         assertTrue(Course.courseExists(course));
+    }
+
+    @Test
+    public void getCourseList() {
+        Course course1 = new Course("CS", 1000, "Intro to Computer Science");
+        Course course2 = new Course("CS", 2000, "DSA");
+        Course course3 = new Course("HS", 1000, "Intro To History");
+        Course course4 = new Course("MATH", 1000, "Intro to Math");
+        Course.insertCourse(course1);
+        Course.insertCourse(course2);
+        Course.insertCourse(course3);
+        Course.insertCourse(course4);
+
+        assertTrue(Course.getCourseList().equals(List.of(course1, course2, course3, course4)));
     }
 
 }
