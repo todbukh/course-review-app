68047e49611a9d7a9c04900819ae0dc1f26cf8ef
Todd <todd@MacBook-Pro.local>
2025-12-06 17:29:35 -0500

    implemented getAllCourses() for retrieving all coursescurrently in the catalog

--- src/main/java/edu/virginia/sde/reviews/CourseCatalogService.java
+++ src/main/java/edu/virginia/sde/reviews/CourseCatalogService.java
@@ -1,7 +1,18 @@
 package edu.virginia.sde.reviews;
 
+import java.util.List;
+
 /**
  * Handles business logic for Course Search Scene
  */
 public class CourseCatalogService {
+    private static List<Course> courses;
+
+    /**
+     * Wrapper method for {@link Course#getCourseList()}.
+     * @return a List of all courses in the catalog
+     */
+    public static List<Course> getAllCourses() {
+        return Course.getCourseList();
+    }
 }
