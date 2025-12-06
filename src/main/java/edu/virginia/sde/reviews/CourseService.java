ea186cbc759b7705f9730d2d3a17a9475749b4d4
Todd <todd@MacBook-Pro.local>
2025-12-06 18:50:25 -0500

    implemented new Course validation

--- src/main/java/edu/virginia/sde/reviews/CourseService.java
+++ src/main/java/edu/virginia/sde/reviews/CourseService.java
@@ -12,6 +12,38 @@ public class CourseService {
     public List<Course> getAllCourses() {
         return Course.getCourseList();
     }
+    public boolean addCourse(String subject, String courseNumberStr, String title) {
+        validateSubjectMnemonic(subject);
+        validateCourseNumberStr(courseNumberStr);
+        validateTitle(title);
+        Course newCourse = new Course(subject.toUpperCase(), Integer.parseInt(courseNumberStr), title);
+        if (Course.courseExists(newCourse)) return false;
+        Course.insertCourse(newCourse);
+        return true;
+    }
+
+    private void validateTitle(String title) {
+        if (title == null) throw new RuntimeException("The course title cannot be null.");
+        title = title.strip();
+        if (title.isEmpty()) throw new RuntimeException("Course title cannot be empty.");
+        if (title.length() > 50) throw new RuntimeException("The course title cannot be longer than 50 characters.");
+    }
+    private void validateSubjectMnemonic(String subject) {
+        if (subject == null) throw new RuntimeException("Subject mnemonic cannot be null.");
+        subject = subject.strip();
+        if (subject.isEmpty()) throw new RuntimeException("Subject mnemonic cannot be empty.");
+        if (!subject.matches("[a-zA-Z]+")) throw new RuntimeException("Invalid subject mnemonic: Only English letters allowed.");
+        if (subject.length() < 2 || subject.length() > 4) throw new RuntimeException("Invalid subject mnemonic: length must be between 2 and 4 characters.");
+    }
+    private void validateCourseNumberStr(String courseNumberStr) {
+        if (courseNumberStr == null) throw new RuntimeException("Course number cannot be null.");
+        courseNumberStr = courseNumberStr.strip();
+        if (courseNumberStr.isEmpty()) throw new RuntimeException("Course number cannot be empty.");
+        if (courseNumberStr.length() != 4) throw new RuntimeException("Course number must be exactly 4 digits.");
+        for (int i = 0; i < courseNumberStr.length(); i++) {
+            if (!Character.isDigit(courseNumberStr.charAt(i))) throw new RuntimeException("Only numeric characters are allowed for course number.");
+        }
+    }
     public List<Course> searchCourses(String subject, String courseNumberStr, String title ) {
         List<Course> allCourses = Course.getCourseList();
         return allCourses.stream()
