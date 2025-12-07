0ebc7170f71548b41004313a31c36a53da37fa25
Todd <todd@MacBook-Pro.local>
2025-12-06 19:11:52 -0500

    added some javadocs to the CourseService methods. Refactored to throw IllegalArgumentException instead of generic RuntimeException if input is invlaid for adding a course.

--- src/main/java/edu/virginia/sde/reviews/CourseService.java
+++ src/main/java/edu/virginia/sde/reviews/CourseService.java
@@ -3,15 +3,28 @@ package edu.virginia.sde.reviews;
 import org.hibernate.Session;
 
 import java.util.*;
-
 /**
  * Handles business logic for Course Search Scene
  */
 public class CourseService {
-
     public List<Course> getAllCourses() {
         return Course.getCourseList();
     }
+
+    /**
+     * Adds a course with a user-specified subject mnemonic, course number, and course title, with the following constraints:
+     * <ul>
+     *      <li> <b>Subject mnemonic</b>: strictly 2-4 letters</li>
+     *      <li> <b>Course number</b>: strictly 4 digits</li>
+     *      <li> <b>Course title</b>: between 1 and 50 characters</li>
+     * </ul>
+     * If any constraint is violated, an exception is thrown.
+     * @param subject a 2-4 letter subject mnemonic
+     * @param courseNumberStr a 4-digit course number
+     * @param title a 1-50 character course title
+     * @throws IllegalArgumentException if input is invalid.
+     * @return {@code true} if course was successfully added, and {@code false} if course already exists
+     */
     public boolean addCourse(String subject, String courseNumberStr, String title) {
         validateSubjectMnemonic(subject);
         validateCourseNumberStr(courseNumberStr);
@@ -20,29 +33,6 @@ public class CourseService {
         if (Course.courseExists(newCourse)) return false;
         Course.insertCourse(newCourse);
         return true;
-    }
-
-    private void validateTitle(String title) {
-        if (title == null) throw new RuntimeException("The course title cannot be null.");
-        title = title.strip();
-        if (title.isEmpty()) throw new RuntimeException("Course title cannot be empty.");
-        if (title.length() > 50) throw new RuntimeException("The course title cannot be longer than 50 characters.");
-    }
-    private void validateSubjectMnemonic(String subject) {
-        if (subject == null) throw new RuntimeException("Subject mnemonic cannot be null.");
-        subject = subject.strip();
-        if (subject.isEmpty()) throw new RuntimeException("Subject mnemonic cannot be empty.");
-        if (!subject.matches("[a-zA-Z]+")) throw new RuntimeException("Invalid subject mnemonic: Only English letters allowed.");
-        if (subject.length() < 2 || subject.length() > 4) throw new RuntimeException("Invalid subject mnemonic: length must be between 2 and 4 characters.");
-    }
-    private void validateCourseNumberStr(String courseNumberStr) {
-        if (courseNumberStr == null) throw new RuntimeException("Course number cannot be null.");
-        courseNumberStr = courseNumberStr.strip();
-        if (courseNumberStr.isEmpty()) throw new RuntimeException("Course number cannot be empty.");
-        if (courseNumberStr.length() != 4) throw new RuntimeException("Course number must be exactly 4 digits.");
-        for (int i = 0; i < courseNumberStr.length(); i++) {
-            if (!Character.isDigit(courseNumberStr.charAt(i))) throw new RuntimeException("Only numeric characters are allowed for course number.");
-        }
     }
     public List<Course> searchCourses(String subject, String courseNumberStr, String title ) {
         List<Course> allCourses = Course.getCourseList();
@@ -66,5 +56,27 @@ public class CourseService {
                         return c.getCourseName().toLowerCase().contains(title.strip().toLowerCase());
                 })
                 .toList();
+    }
+    private void validateTitle(String title) {
+        if (title == null) throw new IllegalArgumentException("The course title cannot be null.");
+        title = title.strip();
+        if (title.isEmpty()) throw new IllegalArgumentException("Course title cannot be empty.");
+        if (title.length() > 50) throw new IllegalArgumentException("The course title cannot be longer than 50 characters.");
+    }
+    private void validateSubjectMnemonic(String subject) {
+        if (subject == null) throw new IllegalArgumentException("Subject mnemonic cannot be null.");
+        subject = subject.strip();
+        if (subject.isEmpty()) throw new IllegalArgumentException("Subject mnemonic cannot be empty.");
+        if (!subject.matches("[a-zA-Z]+")) throw new IllegalArgumentException("Invalid subject mnemonic: Only English letters allowed.");
+        if (subject.length() < 2 || subject.length() > 4) throw new IllegalArgumentException("Invalid subject mnemonic: length must be between 2 and 4 characters.");
+    }
+    private void validateCourseNumberStr(String courseNumberStr) {
+        if (courseNumberStr == null) throw new IllegalArgumentException("Course number cannot be null.");
+        courseNumberStr = courseNumberStr.strip();
+        if (courseNumberStr.isEmpty()) throw new IllegalArgumentException("Course number cannot be empty.");
+        if (courseNumberStr.length() != 4) throw new IllegalArgumentException("Course number must be exactly 4 digits.");
+        for (int i = 0; i < courseNumberStr.length(); i++) {
+            if (!Character.isDigit(courseNumberStr.charAt(i))) throw new IllegalArgumentException("Only numeric characters are allowed for course number.");
+        }
     }
 }
