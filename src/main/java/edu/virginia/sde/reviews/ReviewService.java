50736e6dfad26153bb319a9eb2802502601298fc
Todd <todd@MacBook-Pro.local>
2025-12-08 00:23:52 -0500

    finished implementing most methods. Awaiting some DB methods

--- src/main/java/edu/virginia/sde/reviews/ReviewService.java
+++ src/main/java/edu/virginia/sde/reviews/ReviewService.java
@@ -13,11 +13,9 @@ public class ReviewService {
         SUCCESS
     }
     private final Profile loggedProfile;
-    private List<Review> loggedProfileReviews;
 
     public ReviewService(Profile loggedProfile) {
         this.loggedProfile = loggedProfile;
-        this.loggedProfileReviews = getReviewsByUser(loggedProfile);
     }
 
     public List<Review> getReviewsForCourse(Course course) {
@@ -26,7 +24,7 @@ public class ReviewService {
 
     public ReviewResult submitReview(Course course, int rating, String comment) {
         if (!isRatingValid(rating)) return ReviewResult.FAILED_INVALID_RATING;
-        if (loggedProfileReviews.stream().anyMatch(r -> r.getCourse().equals(course)))
+        if (getLoggedProfileReviews().stream().anyMatch(r -> r.getCourse().equals(course)))
             return ReviewResult.FAILED_USER_ALREADY_REVIEWED;
         String timestamp = new Timestamp(System.currentTimeMillis()).toString();
         Review review = new Review(loggedProfile, course, rating, timestamp, comment);
@@ -41,21 +39,20 @@ public class ReviewService {
     public ReviewResult editReview(int reviewId, int newRating, String newComment) {
         // FIXME: need DB method to update a review given the reviewId
         if (!isRatingValid(newRating)) return ReviewResult.FAILED_INVALID_RATING;
-        // update review with reviewID with the new rating and comment
+        // update the review with reviewID with the new rating and comment
         return ReviewResult.SUCCESS;
     }
 
-    public boolean deleteReview(int reviewId) {
-        // FIXME: implement
-        return false;
+    public void deleteReview(int reviewId) {
+        // FIXME: need DB method to delete a review given the reviewId
+        // delete review with reviewId
     }
 
     public double getCourseAverageRating(Course course) {
-        // FIXME: implement
-        return -1.0;
+        return getReviewsForCourse(course).stream().mapToInt(Review::getRating).average().orElse(0.0);
     }
 
-    public List<Review> getReviewsByUser(Profile user) {
+    public List<Review> getLoggedProfileReviews() {
         //FIXME: need DB method to get Reviews by user
         return null;
     }
