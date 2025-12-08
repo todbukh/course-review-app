fca63923fdae11a00ae2e684572f42074985563b
Todd <todd@MacBook-Pro.local>
2025-12-08 00:38:14 -0500

    refactor ReviewService.java: rounded avg rating to 2dp, and formatted the timestamp string

--- src/main/java/edu/virginia/sde/reviews/ReviewService.java
+++ src/main/java/edu/virginia/sde/reviews/ReviewService.java
@@ -1,7 +1,8 @@
 package edu.virginia.sde.reviews;
 
 import java.util.*;
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Handles business logic for the "My Reviews" and "Course Reviews" Scenes
@@ -17,7 +18,10 @@ public class ReviewService {
     public ReviewService(Profile loggedProfile) {
         this.loggedProfile = loggedProfile;
     }
-
+    public List<Review> getLoggedProfileReviews() {
+        //FIXME: need DB method to get Reviews by user
+        return null;
+    }
     public List<Review> getReviewsForCourse(Course course) {
         return Review.getReviewsFromCourse(course);
     }
@@ -26,20 +30,16 @@ public class ReviewService {
         if (!isRatingValid(rating)) return ReviewResult.FAILED_INVALID_RATING;
         if (getLoggedProfileReviews().stream().anyMatch(r -> r.getCourse().equals(course)))
             return ReviewResult.FAILED_USER_ALREADY_REVIEWED;
-        String timestamp = new Timestamp(System.currentTimeMillis()).toString();
-        Review review = new Review(loggedProfile, course, rating, timestamp, comment);
+        Review review = new Review(loggedProfile, course, rating, comment, getCurrentTimestampString());
         Review.insertReview(review);
         return ReviewResult.SUCCESS;
-    }
-
-    private boolean isRatingValid(int Rating) {
-        return Rating >= 1 && Rating <= 5;
     }
 
     public ReviewResult editReview(int reviewId, int newRating, String newComment) {
         // FIXME: need DB method to update a review given the reviewId
         if (!isRatingValid(newRating)) return ReviewResult.FAILED_INVALID_RATING;
-        // update the review with reviewID with the new rating and comment
+        String timestampString = getCurrentTimestampString();
+        // update the review with reviewID with the new rating, comment, and timestamp
         return ReviewResult.SUCCESS;
     }
 
@@ -49,11 +49,16 @@ public class ReviewService {
     }
 
     public double getCourseAverageRating(Course course) {
-        return getReviewsForCourse(course).stream().mapToInt(Review::getRating).average().orElse(0.0);
+        double avg = getReviewsForCourse(course).stream().mapToInt(Review::getRating).average().orElse(0.0);
+        return Math.round(avg * 100.0) / 100.0;
     }
 
-    public List<Review> getLoggedProfileReviews() {
-        //FIXME: need DB method to get Reviews by user
-        return null;
+    private String getCurrentTimestampString() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm");
+        return LocalDateTime.now().format(dtf);
+    }
+
+    private boolean isRatingValid(int Rating) {
+        return Rating >= 1 && Rating <= 5;
     }
 }
