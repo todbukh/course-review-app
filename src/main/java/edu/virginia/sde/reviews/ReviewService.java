43e3f055c7c5df885e246fc5a14f5e7e2f4263a8
Todd <todd@MacBook-Pro.local>
2025-12-07 23:14:05 -0500

    Added method stubs

--- src/main/java/edu/virginia/sde/reviews/ReviewService.java
+++ src/main/java/edu/virginia/sde/reviews/ReviewService.java
@@ -1,8 +1,38 @@
 package edu.virginia.sde.reviews;
 
+import java.util.*;
+
 /**
  * Handles business logic for the "My Reviews" and "Course Reviews" Scenes
  */
 public class ReviewService {
 
+    public List<Review> getReviewsForCourse(Course course) {
+        // FIXME: implement
+        return null;
+    }
+    public boolean submitReview(Course course, Profile user, int Rating, String comment) {
+        //FIXME: implement
+        return false;
+    }
+
+    public boolean editReview(int reviewId, int newRating, String newComment) {
+        // FIXME: implement
+        return false;
+    }
+
+    public boolean deleteReview(int reviewId) {
+        // FIXME: implement
+        return false;
+    }
+
+    public double getCourseAverageRating(Course course) {
+        // FIXME: implement
+        return -1.0;
+    }
+
+    public List<Review> getReviewsByUser(Profile user) {
+        //FIXME: implement
+        return null;
+    }
 }
