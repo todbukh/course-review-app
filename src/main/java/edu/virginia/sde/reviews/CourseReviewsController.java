aa205cc1f637051bf7bd26e94547b51b3e743581
natestev <gdt2cf@virginia.edu>
2025-12-11 16:47:54 -0500

    Implement controller logic to save and list reviews

--- src/main/java/edu/virginia/sde/reviews/CourseReviewsController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewsController.java
@@ -1,14 +1,63 @@
 package edu.virginia.sde.reviews;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
+import javafx.scene.control.*;
 
 public class CourseReviewsController {
-    @FXML
-    private Label messageLabel;
 
-    public void handleButton() {
-        messageLabel.setText("You pressed the button!");
+    @FXML
+    public Label courseInfoLabel;
+    @FXML
+    public ListView<Review> reviewListView;
+    @FXML
+    public ChoiceBox<Integer> ratingChoiceBox;
+    @FXML
+    public TextArea commentArea;
+    @FXML
+    public Label errorLabel;
+
+    private ReviewService reviewService;
+    private Course currentCourse;
+    private ObservableList<Review> reviewsData = FXCollections.observableArrayList();
+
+    public void initialize() {
+        ratingChoiceBox.setItems(FXCollections.observableArrayList(1, 2, 3, 4, 5));
+        reviewListView.setItems(reviewsData);
+    }
+
+    public void setContext(User user, Course course) {
+        this.currentCourse = course;
+        this.reviewService = new ReviewService(user);
+        courseInfoLabel.setText(course.getSubject() + " " + course.getCourseNumber());
+        refreshReviews();
+    }
+
+    private void refreshReviews() {
+        reviewsData.clear();
+        reviewsData.addAll(reviewService.getReviewsForCourse(currentCourse));
+    }
+
+    @FXML
+    public void handleSave() {
+        if (ratingChoiceBox.getValue() == null) {
+            errorLabel.setText("Please pick a rating.");
+            return;
+        }
+
+        var result = reviewService.submitReview(
+                currentCourse,
+                ratingChoiceBox.getValue(),
+                commentArea.getText()
+        );
+
+        if (result == ReviewService.ReviewResult.SUCCESS) {
+            errorLabel.setText("");
+            commentArea.clear();
+            refreshReviews();
+        } else {
+            errorLabel.setText("Error: " + result.toString());
+        }
     }
 }
