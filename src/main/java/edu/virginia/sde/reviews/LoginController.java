53015515f7aec0586feb850efd04fe7329749178
Kaixuan Zhang <kai.xuan.zhang@outlook.com>
2025-12-11 15:15:02 -0500

    Add appropriate messages for login scene

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -1,6 +1,7 @@
 package edu.virginia.sde.reviews;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -21,11 +22,31 @@ public class LoginController {
         loginButton.setOnAction(e -> handleLogin());
     }
 
+    private static void showAlert(Alert.AlertType alertType, String title, String message){
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+
     @FXML
     public void handleLogin() {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
+        if (username.isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, "Username Empty", "Please enter your username");
+            return;
+        }
+        if (password.isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, "Password Empty", "Please enter your password");
+            return;
+        }
+
         User user = userService.login(username, password);
+        if (user == null) {
+            showAlert(Alert.AlertType.CONFIRMATION, "Login Failed", "Please enter correct username and password");
+        }
     }
 }
