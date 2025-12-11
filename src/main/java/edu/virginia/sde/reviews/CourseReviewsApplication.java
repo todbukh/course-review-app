c31667b4e45d2d3fb91301ac0612018ec02ea277
natestev <gdt2cf@virginia.edu>
2025-12-11 17:19:28 -0500

    Add temporary test class for reviews screen

--- src/main/java/edu/virginia/sde/reviews/CourseReviewsApplication.java
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewsApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.List;
 
 public class CourseReviewsApplication extends Application {
     public static void main(String[] args) {
@@ -12,9 +13,27 @@ public class CourseReviewsApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(CourseReviewsApplication.class.getResource("course-reviews.fxml"));        Scene scene = new Scene(fxmlLoader.load());
+        FXMLLoader fxmlLoader = new FXMLLoader(CourseReviewsApplication.class.getResource("course-reviews.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        if (!User.usernameExists("testUser")) {
+            User.insertUser(new User("testUser", "password123"));
+        }
+        User currentUser = new UserService().login("testUser", "password123");
+
+        Course testCourse = new Course("CS", 3140, "Software Development");
+        if (!Course.courseExists(testCourse)) {
+            Course.insertCourse(testCourse);
+        }
+        List<Course> courses = new CourseService().searchCourses("CS", "3140", null);
+        Course currentCourse = courses.get(0);
+
+        CourseReviewsController controller = fxmlLoader.getController();
+        controller.setContext(currentUser, currentCourse);
+
         stage.setTitle("Course Reviews");
         stage.setScene(scene);
         stage.show();
     }
+
 }
